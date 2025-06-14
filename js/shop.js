7412f654b2873e5b176e473c359c1c6093d22d56
YasminTorresDesign <yatorres.73@gmail.com>
2025-06-13 21:09:47 -0500

    commit inicial

---
+++ js/shop.js
@@ -0,0 +1,117 @@
+// Variables
+const carrito = document.querySelector('#carrito');
+const listaProductos = document.querySelector('#lista-productos');
+const contenedorCarrito = document.querySelector('#lista-carrito tbody');
+const vaciarCarritoBtn = document.querySelector('#vaciar-carrito'); 
+let articulosCarrito = [];
+
+// Listeners
+cargarEventListeners();
+
+function cargarEventListeners() {
+     // Dispara cuando se presiona "Agregar Carrito"
+     listaProductos.addEventListener('click', agregarProducto);
+
+     // Cuando se elimina un curso del carrito
+     carrito.addEventListener('click', eliminarProducto);
+
+     // Al Vaciar el carrito
+     vaciarCarritoBtn.addEventListener('click', vaciarCarrito);
+}
+
+// Funciones
+// Función que añade el curso al carrito
+function agregarProducto(e) {
+    e.preventDefault();
+    // Delegation para agregar-carrito
+    if(e.target.classList.contains('agregar-carrito')) {
+         const producto = e.target.parentElement.parentElement;
+         // Enviamos el producto seleccionado para tomar sus datos
+         leerDatosProducto(producto);
+         
+    }
+}
+
+// Lee los datos del producto
+function leerDatosProducto(producto) {
+    const infoProducto = {
+         imagen: producto.querySelector('img').src,
+         titulo: producto.querySelector('h3').textContent,
+         precio: producto.querySelector('.precio').textContent,
+         id: producto.querySelector('a').getAttribute('data-id'), 
+         cantidad: 1
+    }
+
+
+    if( articulosCarrito.some( producto => producto.id === infoProducto.id ) ) { 
+         const productos = articulosCarrito.map( producto => {
+              if( producto.id === infoProducto.id ) {
+                   producto.cantidad++;
+                    return producto;
+               } else {
+                    return producto;
+            }
+         })
+         articulosCarrito = [...productos];
+    }  else {
+         articulosCarrito = [...articulosCarrito, infoProducto];
+    }
+
+    // console.log(articulosCarrito)
+
+    
+
+    // console.log(articulosCarrito)
+    carritoHTML();
+}
+
+// Elimina el curso del carrito en el DOM
+function eliminarProducto(e) {
+    e.preventDefault();
+    if(e.target.classList.contains('borrar-producto') ) {
+         // e.target.parentElement.parentElement.remove();
+         const productoId = e.target.getAttribute('data-id')
+         
+         // Eliminar del arreglo del carrito
+         articulosCarrito = articulosCarrito.filter(producto => producto.id !== productoId);
+
+         carritoHTML();
+    }
+}
+
+
+// Muestra el curso seleccionado en el Carrito
+function carritoHTML() {
+
+    vaciarCarrito();
+
+    articulosCarrito.forEach(producto => {
+         const row = document.createElement('tr');
+         row.innerHTML = `
+              <td>  
+                   <img src="${producto.imagen}" width=100>
+              </td>
+              <td>${producto.titulo}</td>
+              <td>${producto.precio}</td>
+              <td>${producto.cantidad} </td>
+              <td>
+                   <a href="#" class="borrar-producto" data-id="${producto.id}">X</a>
+              </td>
+         `;
+         contenedorCarrito.appendChild(row);
+    });
+
+}
+
+// Elimina los cursos del carrito en el DOM
+function vaciarCarrito() {
+    // forma lenta
+    // contenedorCarrito.innerHTML = '';
+
+
+    // forma rapida (recomendada)
+    while(contenedorCarrito.firstChild) {
+         contenedorCarrito.removeChild(contenedorCarrito.firstChild);
+     }
+}
+
